b3e7b1165dc66b5f7ae24b16a0920aaabf7529d4
Daniyar Kazbek <daniyar1509@gmail.com>
2026-07-06 11:17:26 +0500

    fix: support multiple GPU architectures and future GPUs in CUDA compilation

--- chamfer_dist_tensorflow/compile.sh
+++ chamfer_dist_tensorflow/compile.sh
@@ -21,11 +21,26 @@ TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.ge
 
 # 2. Compile CUDA Kernels
 echo "[INFO] Compiling CUDA kernels..."
-# TODO add more --gencode targets for other GPU architectures
+
+# Determine CUDA architecture flags.
+# We generate SASS (cubin) for common GPU generations and embed PTX
+# (code=compute_80) so the driver can JIT-compile for future architectures.
+# Adjust or comment out lines for architectures you do not need.
+CUDA_GENCODE_FLAGS=(
+    -gencode arch=compute_60,code=sm_60   # Pascal: GTX 1080 Ti, P100, P40
+    -gencode arch=compute_70,code=sm_70   # Volta: V100, Titan V
+    -gencode arch=compute_75,code=sm_75   # Turing: RTX 2080 Ti, T4, Quadro RTX
+    -gencode arch=compute_80,code=sm_80   # Ampere: A100, A6000, RTX 3090
+    -gencode arch=compute_86,code=sm_86   # Ampere: RTX 3060/3070/3080, A40
+    -gencode arch=compute_89,code=sm_89   # Ada Lovelace: RTX 4090, L40
+    -gencode arch=compute_90,code=sm_90   # Hopper: H100, H200
+    -gencode arch=compute_80,code=compute_80  # PTX fallback for future GPUs (Blackwell+)
+)
+
 nvcc -c -o chamfer_kernels.cu.o chamfer_kernels.cu \
     ${TF_CFLAGS[@]} \
     -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -w --expt-relaxed-constexpr \
-    -gencode arch=compute_80,code=sm_80
+    "${CUDA_GENCODE_FLAGS[@]}"
 
 # 3. Compile and link the C++ code with the CUDA kernels
 echo "[INFO] Compiling and Linking C++ Ops..."
